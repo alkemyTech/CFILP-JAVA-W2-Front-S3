108c4d1d5d0fda2769805e6871834e64d59570a3
Lauty! <lautaronorielasat@gmail.com>
2025-05-21 00:46:51 -0300

    Add more pages

---
+++ src/pages/Accounts.jsx
@@ -0,0 +1,7 @@
+export const Accounts = () => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 p-4">
+      Accounts
+    </div>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
 export { Home } from "./Home";
 export { Login } from "./Login";
 export { Register } from "./Register";
+
+export { Cards } from "./Cards";
+export { Accounts } from "./Accounts";
