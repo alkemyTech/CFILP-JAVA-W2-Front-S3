a2b2f2f2905369a111dcb0bf522af0cedfd5866b
Lauty! <lautaronorielasat@gmail.com>
2025-05-21 01:04:51 -0300

    Fix styles

--- src/components/PageContainer.jsx
+++ src/components/PageContainer.jsx
@@ -2,7 +2,7 @@ import { Header, Navigator } from "./";
 
 export const PageContainer = ({ children }) => {
   return (
-    <div className="w-full h-screen max-h-screen mx-auto overflow-y-auto bg-gray-100 max-w-7xl">
+    <div className="w-full h-screen max-h-screen px-4 mx-auto overflow-y-auto bg-gray-100 max-w-7xl">
       <Header />
 
       <section className="flex flex-col w-full h-full md:h-auto md:grid md:grid-cols-4">
